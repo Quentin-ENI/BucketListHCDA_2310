02ba49b68d8ee76287b58e8c454f44adc8e03f7c
DevFanch <devfanch44@gmail.com>
2024-07-24 16:31:17 +0200

    M05 TP1

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,18 +12,28 @@ use Symfony\Component\Routing\Attribute\Route;
 class WishController extends AbstractController
 {
     #[Route('/', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
+        // FindByCustom() existe pour chaque attribut de l'entité
+
         return $this->render('wish/list.html.twig', [
             'title' => 'Wish List',
+            'wishes' => $wishRepository->findBy([], ['createdAt' => 'DESC'])
         ]);
     }
 
-    #[Route('/detail', name: 'detail')]
-    public function detail(): Response
+    #[Route('/{id<\d+>}', name: 'detail')]
+//    public function detail(int $id, WishRepository $wishRepository): Response
+    public function detail(Wish $wish): Response
     {
+//        $wish = $wishRepository->find($id);
+
+        if (!$wish) {
+            throw $this->createNotFoundException('Ce wish n\'existe pas !!');
+        }
         return $this->render('wish/detail.html.twig', [
             'title' => 'Wish Detail',
+            'wish' => $wish
         ]);
     }
 }
